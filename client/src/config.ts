cdefea5979b49d7b51285f6e537332323457ae31
Foluwa <moronfoluwaakintola@gmail.com>
2022-08-20 23:02:21 +0400

    Init state

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'hb9l2u0904';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'errandx.auth0.com',            // Auth0 domain
+  clientId: 'nke5v60OnNs1vHI9hx7aDVI0I36CltSu',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+};
